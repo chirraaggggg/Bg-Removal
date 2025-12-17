41603646f2fbddf5fcf9f9b59f81ee5f54a402b8
Chirag <chhirag920@gmail.com>
2025-12-17 10:57:56 +0530

    added the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import { assets } from '../assets/assets'
 const Header = () => {
   return (
-    <div>
+    <div className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
       {/* ---------- Left side ---------- */}
       <div>
-        <h1>Remove the <br /> <span>background</span> from <br /> images for free.</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /> Lorem Ipsum has been the industry's standard dummy text ever.</p>
-        <div>
-            <input type="file" name="" id="upload1" hidden />
-            <label htmlFor="upload1">
-                <img src={assets.upload_btn_icon} alt="" />
-                <p>Upload yout image</p>
-            </label>
+        <h1 className='text-4xl xl:text-5xl 2xl:text-6xl font-bold text-natural-700 leading-tight'>
+            Remove the <br className='max-md:hidden'/> <span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>background</span> from <br className='max-md:hidden'/> images for free.
+        </h1>
+        <p className='my-6 text-[15px] text-gray-500'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br className='max-sm:hidden'/> Lorem Ipsum has been the industry's standard dummy text ever.</p>
+        <div className='mt-4'>
+          <input type="file" name='' id="upload1" hidden />
+          <label
+            htmlFor="upload1"
+            className='inline-flex gap-4 px-8 py-3.5 rounded-full items-center cursor-pointer bg-gradient-to-r from-violet-600 from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700'
+          >
+            <img width={28} height={28} src={assets.upload_btn_icon} alt="Upload" />
+            <p className='text-white text-sm'>Upload your image </p>
+          </label>
         </div>
       </div>
       {/* ---------- Left side ---------- */}
-      <div>
-
+      <div className='w-full max-w-md'>
+        <img src={assets.header_img} alt="" />
       </div>
     </div>
   )
