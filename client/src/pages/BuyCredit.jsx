8b784f10d6db186fea9e4983a8bb16d39193c2c5
Chirag <chhirag920@gmail.com>
2026-01-03 22:21:31 +0530

    pushing ny code

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,9 +1,24 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
 const BuyCredit = () => {
 	return (
-		<main>
-			<h1>Buy Credits</h1>
-			<p>Purchase credits to remove more backgrounds.</p>
-		</main>
+		<div className="min-h-[80vh] text-center pt-14 mb-10">
+			<button>Our Plans</button>
+			<h1>Choose the plan that's right for you</h1>
+			<div>
+				{plans.map((item,index)=>(
+					<div>
+						<img width={40} src={assets.logo_icon} alt="" />
+						<p>{item.id}</p>
+						<p>{item.desc}</p>
+						<p>
+							<span>${item.price}</span>/ {item.credits} credits
+						</p>
+					</div>
+				))}
+			</div>
+		</div>
 	)
 }
 
