f359c537a8e808704a6c65f710239cf519e8c67b
Chirag <chhirag920@gmail.com>
2026-01-07 15:10:53 +0530

    pushing the code

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, plans } from '../assets/assets'
 
 const BuyCredit = () => {
 	return (
 		<div className="min-h-[80vh] text-center pt-14 mb-10">
-			<button>Our Plans</button>
-			<h1>Choose the plan that's right for you</h1>
+			<button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
+			<h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>Choose the plan that's right for you</h1>
 			<div>
 				{plans.map((item,index)=>(
 					<div>
@@ -15,6 +15,7 @@ const BuyCredit = () => {
 						<p>
 							<span>${item.price}</span>/ {item.credits} credits
 						</p>
+						<button>Purchase</button>
 					</div>
 				))}
 			</div>
