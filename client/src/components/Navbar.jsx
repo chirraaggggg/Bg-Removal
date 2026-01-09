bc1e3da85dc3b18dc12d6c66cf9d2ce08c1b946c
Chirag <chhirag920@gmail.com>
2026-01-09 19:50:31 +0530

    added user button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { Link } from 'react-router-dom'
-import { useClerk } from '@clerk/clerk-react'
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+import { useClerk } from "@clerk/clerk-react";
+import { UserButton } from '@clerk/clerk-react';
+import { useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
-
   const { openSignIn } = useClerk();
-  return (
-    <div className='flex items-center justify-between mx-4 py-3 lg:mx-44'>
-      <Link to="/"><img className='w-32 sm:w-44' src={assets.logo} alt="" /></Link>
-      <button onClick={ openSignIn }className='bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full'>
-        Get started <img className='w-3 sm:w-4' src={assets.arrow_icon} alt="" />
-      </button>
-    </div>
-  )
-}
+  const { isSignedIn, user } = useUser();
 
-export default Navbar
+  return (
+    <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
+      <Link to="/">
+        <img className="w-32 sm:w-44" src={assets.logo} alt="" />
+      </Link>
+      {isSignedIn ?
+        <div>
+          <UserButton />
+        </div>
+      :
+        <button
+          onClick={openSignIn}
+          className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full"
+        >
+          Get started{" "}
+          <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+        </button>
+      }
+    </div>
+  );
+};
+
+export default Navbar;
