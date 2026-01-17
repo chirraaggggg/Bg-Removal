c72ad280298003334635493af6947ceff22c27a2
Chirag <chhirag920@gmail.com>
2026-01-18 00:54:05 +0530

    pushing the code

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,8 +1,21 @@
-
+import { Webhook } from "svix"
 
 // API controller function to manage clerk user with database
 // http://localhost:4000/api/user/webhooks
 
 const clerkwebhooks = async (req,res) => {
     
+    try {
+
+        // create a svix instance with clerk webhook secret.
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
+    } catch (error) {
+        
+    }
 }
